8b66406818e45b8641d97271621317bcfef040f9
SUJAL RAJPUT <sujalrajput10@gmail.com>
2025-02-12 23:06:45 +0530

    clean up

--- extract_data_by_features.py
+++ extract_data_by_features.py
@@ -13,9 +13,15 @@ features = ["Chl-a", "TURB", "TSS", "DOC", "TP", "TN", "O2-Dis", "EC", "TDS"]
 
 # Load dataset
 df = pd.read_csv("data/Gemstat_Data_Cleaned_Raw/All_lakes_clustered_haversine.csv")
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+
+# Filter data after 2018
+df_after_2018 = df[df["Date"] > "2017-12-31"]
 
 # Process and save top clusters for each feature
 for feature in features:
-    process_and_save_top_clusters(df, feature)
+    process_and_save_top_clusters(
+        df_after_2018, featurebase_path="data/Gemstat_Data_Cleaned_2018"
+    )
 
 print("Processing complete for all features.")
